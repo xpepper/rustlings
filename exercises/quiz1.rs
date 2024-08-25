bed4515786cae7196fce1e7a1860b4429035934e
Pietro Di Bello <pierodibello@gmail.com>
2024-08-25 16:04:52 +0200

    feat: complete the ifs section

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    // 35 => 70 (*2)
+    // 40 => 80 (*2)
+    // 41 => 41 (*1)
+    // 65 => 65 (*1)
+    if apples <= 40 {
+        apples * 2
+    } else {
+        apples
+    }
+}
 
 // Don't modify this function!
 #[test]
